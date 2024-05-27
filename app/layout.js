8c8fe9d2c3e9809d7462b7e653b06f24a3f4bca1
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-05-27 17:35:43 +0700

    fix: change icon js

--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,9 @@ export default function RootLayout({ children }) {
       <body >
         <Header />
         <Body>
-          <ThemeProvider>
+          {/* <ThemeProvider> */}
             {children}
-          </ThemeProvider>
+          {/* </ThemeProvider> */}
         </Body>
         <Footer />
       </body>
