790850fdf0fb8195a8f7fb3a46c0c2f2f172ee57
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-06-16 13:56:14 +0700

    update auth button

--- components/auth/auth-button.jsx
+++ components/auth/auth-button.jsx
@@ -52,65 +52,24 @@ export default function AuthButton() {
   // )
   return (
     <>
-      {/* {status !== "loading" &&
-        (session?.user ? (
-          <Link href={`/${session.username}`}>
-            <a className="w-8 h-8 rounded-full overflow-hidden">
-              <Image
-                src={
-                  session.user.image ||
-                  `https://avatar.tobi.sh/${session.user.name}`
-                }
-                alt={session.user.name || "User"}
-                width={300}
-                height={300}
-                placeholder="blur"
-                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2PYsGHDfwAHNAMQumvbogAAAABJRU5ErkJggg=="
-              />
-            </a>
-          </Link>
-        ) : (
-          <button
-            disabled={loading}
-            onClick={() => {
-              setLoading(true)
-              signIn("github", { callbackUrl: `/profile` })
-            }}
-            className={`${
-              loading
-                ? "bg-gray-200 border-gray-300"
-                : "bg-black hover:bg-white border-black"
-            } w-36 h-8 py-1 text-white hover:text-black border rounded-md text-sm transition-all`}
-          >
-            {loading ? <Loading color="gray" /> : "Log in with GitHub"}
-          </button>
-        ))} */}
-      {/* <form
-        action={async () => {
-          "use server"
-          await signIn("github")
-        }}
-      >
-        <button type="submit">Signin with GitHub</button>
-      </form> */}
       {status !== "loading" &&
         (session?.user ? (
-          <Link href={`/${session.username}`}>
-            <a className="w-8 h-8 rounded-full overflow-hidden">
-              {/* <Image
-                src={
-                  session.user.image ||
-                  `https://avatar.tobi.sh/${session.user.name}`
-                }
-                alt={session.user.name || "User"}
-                width={300}
-                height={300}
-                placeholder="blur"
-                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2PYsGHDfwAHNAMQumvbogAAAABJRU5ErkJggg=="
-              /> */}
-              {session.user.name || "User"}
-            </a>
-          </Link>
+          <div className="flex items-center gap-4">
+            <span
+              className="text-sm font-medium subpixel-antialiased contrast-more:text-gray-700 contrast-more:dark:text-gray-100"
+              aria-current="true"
+            >
+              Hi, {session.user.name || "User"}
+            </span>
+            <form action={signOut}>
+              <button
+                className="text-sm p-2 contrast-more:text-gray-700 contrast-more:dark:text-gray-100 max-md:hidden whitespace-nowrap font-semibold subpixel-antialiased hover:underline"
+                aria-current="true"
+              >
+                Logout
+              </button>
+            </form>
+          </div>
         ) : (
           <button
             disabled={loading}
