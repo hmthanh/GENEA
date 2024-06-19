5366676310d4eac9b21e0c60cf984074d9fd2f09
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-06-19 11:40:20 +0700

    complete upload

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -65,56 +65,57 @@ export async function POST(req, res) {
   const db = client.db("HemVip")
 
   const submissions = []
-  for (let [key, value] of formData.entries()) {
-    if (key === "video") {
-      const arrayBuffer = await value.arrayBuffer()
-      const uniqueKey = `genea/${userId}/${Date.now()}_${value.name}`
-      console.log(uniqueKey)
-
-      // Create the parameters for the PutObjectCommand
-      const params = {
-        Bucket: "gesture",
-        Key: uniqueKey,
-        Body: new Uint8Array(arrayBuffer),
-        ContentType: value.type,
-      }
-
-      const parallelUploads3 = new Upload({
-        client: s3,
-        params: params,
-        leavePartsOnError: false, // Whether to abort the multipart upload on error
-      })
-
-      parallelUploads3.on("httpUploadProgress", (progress) => {
-        console.log("progress", progress)
-      })
-
-      const uploadResult = await parallelUploads3.done()
-      console.log("Upload complete:", uploadResult)
-
-      const filename = value.name.split(".")
-      if (filename.length > 1) {
-        filename.pop()
-      }
-      const inputid = filename.join(".")
-      submissions.push({
-        inputid: inputid,
-        videoid: uploadResult.Key,
-        teamid: userId,
-        url: uploadResult.Location,
-      })
-
-      // Upload the video file to B2 storage
-      // await s3.send(new PutObjectCommand(putObjectParams))
-    }
-  }
-
-  const insertResult = await db
-    .collection("submissions")
-    .insertOne({ userId, teamname, email, submissions })
-  console.log("insertResult", insertResult)
 
   try {
+    for (let [key, value] of formData.entries()) {
+      if (key === "video") {
+        const arrayBuffer = await value.arrayBuffer()
+        const uniqueKey = `genea/${userId}/${Date.now()}_${value.name}`
+        console.log("Uploading uniqueKey", uniqueKey)
+
+        // Create the parameters for the PutObjectCommand
+        const params = {
+          Bucket: "gesture",
+          Key: uniqueKey,
+          Body: new Uint8Array(arrayBuffer),
+          ContentType: value.type,
+        }
+
+        const parallelUploads3 = new Upload({
+          client: s3,
+          params: params,
+          leavePartsOnError: false, // Whether to abort the multipart upload on error
+        })
+
+        parallelUploads3.on("httpUploadProgress", (progress) => {
+          console.log("progress", progress)
+        })
+
+        const uploadResult = await parallelUploads3.done()
+        // console.log("Upload complete:", uploadResult)
+
+        const filename = value.name.split(".")
+        if (filename.length > 1) {
+          filename.pop()
+        }
+        const inputid = filename.join(".")
+        submissions.push({
+          inputid: inputid,
+          videoid: uploadResult.Key,
+          teamid: userId,
+          url: uploadResult.Location,
+        })
+
+        // Upload the video file to B2 storage
+        // await s3.send(new PutObjectCommand(putObjectParams))
+      }
+    }
+
+    const insertResult = await db
+      .collection("submissions")
+      .insertOne({ userId, teamname, email, submissions })
+    console.log("insertResult", insertResult)
+
     if (insertResult.insertedId) {
       return Response.json(
         {
@@ -144,95 +145,4 @@ export async function POST(req, res) {
       { status: 500 }
     )
   }
-
-  // videos.map((video) => {
-  //   console.log(video)
-  // })
-
-  // const putObjectParams = {
-  //   Bucket: "gesture", // Use the bucket created or an existing bucket
-  //   Key: `genea/${fileName}`,
-  //   Body: fileContent,
-  //   ContentType: contentType,
-  // }
-
-  // form.parse(req, async (err, fields, files) => {
-  //   if (err) {
-  //     console.error("Error parsing form data:", err)
-  //     return res
-  //       .status(500)
-  //       .json({ success: false, message: "Error parsing form data" })
-  //   }
-
-  //   try {
-  //     const uploadPromises = Object.values(files)
-  //       .flat()
-  //       .map(async (file) => {
-  //         const fileContent = fs.readFileSync(file.filepath)
-  //         const contentType = file.mimetype
-  //         const fileName = `${uuid()}-${file.originalFilename}`
-
-  //         return s3.send(new PutObjectCommand(putObjectParams))
-  //       })
-
-  //     await Promise.all(uploadPromises)
-
-  //     res
-  //       .status(200)
-  //       .json({ success: true, message: "Files uploaded successfully" })
-  //   } catch (uploadError) {
-  //     console.error("Error uploading files:", uploadError)
-  //     res.status(500).json({ success: false, message: "Error uploading files" })
-  //   }
-  // })
-
-  // req.on("error", (error) => {
-  //   console.error("Error processing request:", error)
-  //   res
-  //     .status(500)
-  //     .json({ success: false, message: "Error processing request" })
-  // })
-
-  // // const multerMiddleware = upload.array("files")
-
-  // // const files = request.files
-  // // const userId = request.body.get("userId")
-
-  // // // Process the parsed files and userId as needed
-  // // console.log("Parsed files:", files)
-  // // console.log("User ID:", userId)
-
-  // // Make a request to get the specified object from the bucket
-  // // Upload an object to the bucket to further validate authentication
-  // try {
-  //   const putObjectParams = {
-  //     Bucket: "gesture", // Use the bucket created or an existing bucket
-  //     Key: "genea/test-object.txt",
-  //     Body: "This is a test object.",
-  //   }
-
-  //   await s3.send(new PutObjectCommand(putObjectParams))
-  //   console.log("Object uploaded successfully. Authentication confirmed.")
-  //   return Response.json(
-  //     {
-  //       success: true,
-  //       msg: "Object uploaded successfully.",
-  //       error: null,
-  //     },
-  //     { status: 200 }
-  //   )
-  // } catch (error) {
-  //   console.error("Error uploading object:", error)
-  //   console.error(
-  //     "Authentication failed. Check your credentials and permissions."
-  //   )
-  //   return Response.json(
-  //     {
-  //       success: false,
-  //       msg: "Authentication failed. Check your credentials and permissions.",
-  //       error: null,
-  //     },
-  //     { status: 500 }
-  //   ) // Exit the function if authentication fails
-  // }
 }
