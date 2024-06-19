3e6695c0208c4384d63305e7d9cc7b845e33995e
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-06-19 09:53:13 +0700

    remove api

--- app/api/experiment/route.js
+++ app/api/experiment/route.js
@@ -1,16 +1,16 @@
 import axios from "axios"
 
 export async function GET() {
-  const res = await axios.get(
-    "https://raw.githubusercontent.com/hmthanh/GENEA/main/public/my_first_experiment/1234567.json",
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-  // const data = res
-  // console.log('res', res)
+  // const res = await axios.get(
+  //   "https://raw.githubusercontent.com/hmthanh/GENEA/main/public/my_first_experiment/1234567.json",
+  //   {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   }
+  // )
+  // // const data = res
+  // // console.log('res', res)
 
   return Response.json({ data: {} })
 }
