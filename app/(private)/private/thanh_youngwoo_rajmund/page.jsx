187ef41823f3c91382a3462f9e55bbd63b59c07c
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-06-19 18:39:17 +0700

    add generate page

--- app/(private)/private/thanh_youngwoo_rajmund/page.jsx
+++ app/(private)/private/thanh_youngwoo_rajmund/page.jsx
@@ -1,16 +1,19 @@
 "use client"
 
 import axios from "axios"
-import React from "react"
+import React, { useState } from "react"
 
 export default function Page() {
+  const [submission, setSubmission] = useState("")
+  const [studies, setStudies] = useState("")
   const handleGenerate = async () => {
     const result = await axios.get("/api/generate")
+    setSubmission(JSON.stringify(result.data.data, null, 2))
     console.log("result", result)
   }
 
   return (
-    <div>
+    <div className="flex flex-col gap-3">
       <h1 className="mt-2 text-4xl font-bold tracking-tight text-slate-900 dark:text-slate-100">
         Generate all study screen
       </h1>
@@ -25,6 +28,34 @@ export default function Page() {
       >
         Generate All
       </button>
+
+      <div className="flex flex-row items-center gap-4">
+        <label htmlFor="submission" className="flex justify-end w-[15%]">
+          Submission
+        </label>
+        <textarea
+          className="flex-grow min-w-0 appearance-none rounded-md border border-[#666666] bg-white px-4 py-2 text-base text-gray-900 placeholder-gray-500 focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:border-[#888888] dark:bg-transparent dark:text-white dark:focus:border-white sm:text-sm"
+          id="submission"
+          rows="20"
+          name="submission"
+          value={submission}
+          onChange={(e) => setSubmission(e.target.value)}
+        />
+      </div>
+
+      <div className="flex flex-row items-center gap-4">
+        <label htmlFor="studies" className="flex justify-end w-[15%]">
+          Studies
+        </label>
+        <textarea
+          className="flex-grow min-w-0 appearance-none rounded-md border border-[#666666] bg-white px-4 py-2 text-base text-gray-900 placeholder-gray-500 focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:border-[#888888] dark:bg-transparent dark:text-white dark:focus:border-white sm:text-sm"
+          id="studies"
+          rows="20"
+          name="studies"
+          value={studies}
+          onChange={(e) => setStudies(e.target.value)}
+        />
+      </div>
     </div>
   )
 }
